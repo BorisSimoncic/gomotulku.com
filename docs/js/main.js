d23b76fb5df378ed2dead3f35bddba59f33b22ef
BorisSimoncic <boriss@zaslon-telecom.si>
2023-04-30 22:56:01 +0200

    .

--- docs/js/main.js
+++ docs/js/main.js
@@ -259,8 +259,7 @@
                     items: 2,
                 },
                 1200 : {
-                    items: 3,
-                    nav: false
+                    items: 3
                 }
             }
         });
